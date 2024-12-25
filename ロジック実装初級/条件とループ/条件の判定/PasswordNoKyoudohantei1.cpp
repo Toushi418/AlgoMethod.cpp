efe4b2bef0a198b7c0cfd125818ff630f5403a5f
getexi <getegakuen@gmail.com>
2024-12-26 00:41:35 +0900

    [add] アルゴリズム初級実装

---
+++ ロジック実装初級/条件とループ/条件の判定/PasswordNoKyoudohantei1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+
+    if (S == "password"){
+        cout << "dangerous" << endl;
+    }else{
+        cout << "safe" << endl;
+    }
+}
